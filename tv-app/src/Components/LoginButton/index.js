d901d02f4a37b8c86ef42b66d31a4ea6e5bd3e02
RobBuilds <152198994+RobBuilds@users.noreply.github.com>
2024-01-23 15:30:20 -0500

    removed auth0

--- tv-app/src/Components/LoginButton/index.js
+++ tv-app/src/Components/LoginButton/index.js
@@ -3,10 +3,8 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 export default function LoginButton() {
 
-  const { loginWithRedirect } = useAuth0()
-
   return (
-    <button onClick={() => loginWithRedirect()}>
+    <button >
       Log In To Add Items To Watchlist
     </button>
   )
